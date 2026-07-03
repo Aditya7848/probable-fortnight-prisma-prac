c22b2c20f5de8eb4eef2f21b6db63a9aaa4673d5
Aditya7848 <adityas7848@gmail.com>
2026-07-04 03:33:48 +0530

    err:any

--- index.ts
+++ index.ts
@@ -1,16 +1,15 @@
 import express from 'express';
-import {Pool} from 'pg';
-import dotenv from 'dotenv'
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
 
 const app = express();
 
-dotenv.config()
-app.use(express.json())
+dotenv.config();
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello from server.')
-})
-
+  res.send('Hello from server.');
+});
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -23,16 +22,14 @@ async function checkConnection() {
     const res = await client.query('SELECT NOW();');
     console.log('✅ Connected! Server time:', res.rows[0].now);
     client.release();
-  } catch (err) {
+  } catch (err: any) {
     console.error('❌ Connection error:', err.stack);
   }
 }
 checkConnection();
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log(`server is listening in on ${PORT}`)
-})
-
-
+  console.log(`server is listening in on ${PORT}`);
+});
